e86cf9988f9716c91d2aa664f193cdfafd222ca1
Alibek Ashirali <ashiralialibek@gmail.com>
2022-11-26 12:03:15 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-# provider "aws" {
-#     region = "us-east-1"
-# }
-
-# resource "aws_vpc" "myvpc" {
-#     cidr_block = "10.0.0.0/16"
-
-#     tags = {
-#         Name = "ExampleAppServerInstance"
-#     }
-# }
-
-
 terraform {
   required_providers {
     aws = {
