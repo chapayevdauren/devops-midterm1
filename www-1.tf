c1f6a66c557a60672fe5b4858a040bff300c2ccc
DiasKaipan <63948100+DiasKaipan@users.noreply.github.com>
2022-11-26 11:40:57 +0600

    ADD terraform

---
+++ www-1.tf
@@ -0,0 +1,39 @@
+resource "digitalocean_droplet" "www-1" {
+  image = "ubuntu-20-04-x64"
+  name = "www-1"
+  region = "nyc3"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+
+  
+  
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y nginx"
+    ]
+  }
+}
+
+
+resource "digitalocean_database_cluster" "postgres-example" {
+  name       = "example-postgres-cluster"
+  engine     = "pg"
+  version    = "11"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+}
+
